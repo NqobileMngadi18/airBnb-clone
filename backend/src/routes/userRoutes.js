2ef3f2826ce536a405c9dd6d7c94700907388bbf
NqobileMngadi18 <your_email@example.com>
2025-11-13 00:00:57 +0200

    Resolved merge conflicts in backend

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,24 +3,15 @@ const router = express.Router();
 const { body, validationResult } = require("express-validator");
 const auth = require("../middleware/auth");
 const userController = require("../controllers/userController");
-<<<<<<< HEAD
 
 router.post("/login",
     [
         body("email").notEmpty().withMessage("Email or username is required"),
-=======
-const { body } = require("express-validator");
-
-router.post("/login",
-    [
-        body("email").isEmail().withMessage("Valid email is required"),
->>>>>>> 865287e79966027f0cd7f3e715612a2ead50933f
         body("password").notEmpty().withMessage("Password is required"),
     ],
     userController.loginUser
 );
 
-<<<<<<< HEAD
 router.post("/register",
     [
         body("email").isEmail().withMessage("Valid email is required"),
@@ -31,8 +22,6 @@ router.post("/register",
 );
 
 
-=======
->>>>>>> 865287e79966027f0cd7f3e715612a2ead50933f
 router.get("/profile", auth, userController.getUserProfile);
 
 module.exports = router;
